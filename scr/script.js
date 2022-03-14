662365e210860767a294bc0a8b12a62e07820cae
fdejesuscorrea <fdejesus.correa@udea.edu.co>
2022-03-14 16:53:04 -0500

    intento de solucion

--- scr/script.js
+++ scr/script.js
@@ -7,4 +7,20 @@ function copy(e){
     var copy = e.parentElement.firstChild;
     navigator.clipboard.writeText(copy.innerHTML);
 }
-var fs =require("fs");
+let input=document.querySelector('#file');
+
+input.addEventListener('change',()=>{
+    let files =input.files;
+    if(files.length==0) return;
+    const file = files[0];
+    let reader= new FileReader();
+    reader.onload=(e)=>{
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        console.log(lines.join('\n'));
+    }
+    reader.onerror =(e)=>alert(e.target.error.name);
+    reader.readAsText(file);
+}
+
+);
