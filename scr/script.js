e39cd71ca93f4673b4d6a8c7bac707eb10f55837
fdejesuscorrea <fdejesus.correa@udea.edu.co>
2022-03-16 16:34:29 -0500

    eliminacion de codigo duplicado e inserscion de los links reales

--- scr/script.js
+++ scr/script.js
@@ -1,26 +1,11 @@
 function visit(e){
-    var link = e.innerHTML;
+    var link = e.getAttribute("link");
     window.open(link, '_blank');
-    e.className="visitedbutton";
-}
+    var last = document.querySelector(".lastvisitedbutton");
+    if(last) last.className="visitedbutton";
+    e.className="lastvisitedbutton";
+}   
 function copy(e){
     var copy = e.parentElement.firstChild;
     navigator.clipboard.writeText(copy.innerHTML);
 }
-let input=document.querySelector('#file');
-
-input.addEventListener('change',()=>{
-    let files =input.files;
-    if(files.length==0) return;
-    const file = files[0];
-    let reader= new FileReader();
-    reader.onload=(e)=>{
-        const file = e.target.result;
-        const lines = file.split(/\r\n|\n/);
-        console.log(lines.join('\n'));
-    }
-    reader.onerror =(e)=>alert(e.target.error.name);
-    reader.readAsText(file);
-}
-
-);
